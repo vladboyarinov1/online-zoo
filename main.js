4ac15f82814f4cd8268890a72c3e699560a4787b
vladboyarinov1 <97247234+vladboyarinov1@users.noreply.github.com>
2022-09-27 00:19:24 +0300

    feat: add project

---
+++ main.js
@@ -0,0 +1,15 @@
+// const slides = document.querySelectorAll('.testimonials__slide'),
+//       slidesWrapper = document.querySelector('.testimonials__slider-wripper'),
+//       slidesField = document.querySelector('.testimonials__slider-inner'),
+//       width = window.getComputedStyle(slidesWrapper).width;
+// let offset = 0;
+
+// slidesField.style.width = 100 * slides.lenght + '%';
+// slidesField.style.display = 'flex';
+// slidesField.style.transition = '0.5s all';
+
+// slidesWrapper.style.overflow = 'hidden';
+
+// slides.forEach(slide => {
+//     slide.style.width = width;
+// });
