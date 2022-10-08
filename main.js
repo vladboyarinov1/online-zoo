8e6cd9173f384c1d738b3a6893322e6640a8f637
vladboyarinov1 <97247234+vladboyarinov1@users.noreply.github.com>
2022-10-08 16:09:03 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -9,6 +9,51 @@ if (iconMenu) {
         menuBody.classList.toggle('_active');
     });
 }
+//Modal
+
+const modalTrigger  = document.querySelectorAll('.testimonials__slide'),
+      modal = document.querySelector('.testimonials__modal'),
+      modalCloseBtn = document.querySelector('.testimonials__modal__body__close'),
+      bodyBlur = document.querySelector('body');
+     
+modalTrigger.forEach(click => {
+    click.addEventListener('click', () => {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        bodyBlur.classList.add('_blur')
+    });
+});
+
+    modalCloseBtn.addEventListener('click', () => {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        bodyBlur.classList.remove('_blur')
+});
+
+modal.addEventListener('click', (e) => {
+    if(e.target == modal){
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        bodyBlur.classList.remove('_blur');
+    }
+});
 
 
+const swiper = new Swiper('.swiper-container', {
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable:true,
+    },
+    grabCursor:true,
+    breakpoints:{
+       
+        1001:{
+            slidesPerView:4,
+        },
+        1000:{
+            slidesPerView:3,
+        },
+    },
+    loop:true,
 
+  });
